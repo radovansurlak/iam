589199de84ad1bde2ef7f8bdee9619b9881e9e9c
radovansurlak <radovansurlak@gmail.com>
2018-04-16 22:08:56 +0100

    fixed captions bug

--- puppeteerTest.js
+++ puppeteerTest.js
@@ -44,7 +44,7 @@ const axios = require('axios');
 	await page.goto('https://www.eyeem.com/a/13076706');
 	await page.click('#gridPhoto-0 > a:first-child')
 
-	for (let i = 0; i <= 5; i++) {
+	for (let i = 0; i <= 200; i++) {
 		tags = await page.evaluate(async () => [...document.querySelectorAll(".g_bubble")].map(tag => tag.innerText.toLowerCase()));
 
 		let imageUrl = await page.evaluate(() => document.querySelector('.sc-jwKygS.khmKrD').firstChild.style.backgroundImage.match(/"(h.*)"/)[1]);
@@ -68,7 +68,7 @@ const axios = require('axios');
 		imageData.captions = [...imageData.captions, ...result.data.responses[0].captions.map(caption => caption.text.toLowerCase())];
 
 		await page.evaluate(() => document.querySelector('.pview_stage_contextBeltImage-active').parentElement.nextSibling.click()); // Navigate to the next image
-		await page.waitFor(2000);
+		await page.waitFor(1000);
 	}
 	// imageData.aestheticScore = result.data.responses[0].aesthetic_score.score;
 	// Count tags
@@ -102,6 +102,7 @@ const axios = require('axios');
 	let output = {
 		organicTags: sortedScrapedTags,
 		analysedTags: sortedAnalysedTags,
+		captions: imageData.captions,
 	}
 
 	debugger;
